2eb43f16a12bf8722561755c578c9c31bff1f548
efdalyalcin <efdalyalcin@hotmail.com>
2022-08-22 12:03:42 +0300

    about section introduction is done

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,17 @@ import "./About.scss";
 export default function About() {
   return (
     <section className="About" id="About">
-      About
+      <h2>About Me</h2>
+      <div>
+        <p>
+          Hello, I am Efdal and my passion is to create responsive and interactive
+          web pages. User experience, beautiful pixels and clean, readable and reusable code matters for me.
+        </p>
+        <p>
+          I started coding as a hobby which in time possessed me. I left my career to do what I love doing. 
+          
+        </p>
+      </div>
     </section>
   );
 }
