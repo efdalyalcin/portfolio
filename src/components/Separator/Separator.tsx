dad48fc7a4344f67fdaff76656ae52602d483f6a
efdalyalcin <efdalyalcin@hotmail.com>
2022-08-23 20:12:42 +0300

    separator scroll works but increases from 0

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -1,4 +1,4 @@
-import { createRef, useEffect, useState } from 'react';
+import { createRef, useCallback, useEffect, useState } from 'react';
 import cn from 'classnames';
 import { useTheme } from '../../context/ThemeProvider';
 import './Separator.scss';
@@ -7,31 +7,53 @@ export default function Separator() {
   const { isDarkTheme } = useTheme();
   const [scrollPosition, setScrollPosition] = useState(0);
   const sepRef = createRef<HTMLDivElement>();
+  const [separatorPosition, setSeparatorPosition] = useState(1);
+  const [widthPercent, setWidthPercent] = useState(scrollPosition / separatorPosition);
 
-  const handleScroll = () => {
-    let scroll = window.pageYOffset;
-
+  const handleSeparatorPosition = () => {
     console.log(sepRef.current?.offsetTop)
 
-    scroll
-      ? setScrollPosition(scroll)
-      : setScrollPosition(0)
+    sepRef.current?.offsetTop
+      ? setSeparatorPosition(sepRef.current.offsetTop)
+      : setSeparatorPosition(1)
   };
 
+  
+  const handleScroll = () => {
+    let scroll = window.pageYOffset;
+    
+    scroll
+    ? setScrollPosition(scroll)
+    : setScrollPosition(0)
+  };
+  
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-
+    
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   });
-
-  console.log(scrollPosition);
+  
+  const styleHandler = () => {
+    setWidthPercent((scrollPosition / separatorPosition) * 100)
+  };
+  
+  useEffect(
+    () => {
+      handleSeparatorPosition();
+      styleHandler();
+    },
+    [scrollPosition],
+  );
+  console.log(widthPercent);
 
   return (
     <div 
+      style={{"width": `${widthPercent}%`}}
       className={cn("Separator", {"Separator--dark": isDarkTheme})}
       ref={sepRef}
+      onScroll={styleHandler}
     />
   )
 }
